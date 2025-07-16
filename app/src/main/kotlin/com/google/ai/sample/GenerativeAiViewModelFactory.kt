e1e767edba993079a47431af5fecefb22386ae0c
Android PowerUser <88908510+Android-PowerUser@users.noreply.github.com>
2025-07-16 02:50:54 +0200

    Update GenerativeAiViewModelFactory.kt

--- app/src/main/kotlin/com/google/ai/sample/GenerativeAiViewModelFactory.kt
+++ app/src/main/kotlin/com/google/ai/sample/GenerativeAiViewModelFactory.kt
@@ -15,7 +15,7 @@ enum class ModelOption(val displayName: String, val modelName: String) {
     GEMINI_FLASH("Gemini 2.0 Flash", "gemini-2.0-flash"),
     GEMINI_FLASH_LITE_PREVIEW("Gemini 2.5 Flash Lite Preview", "gemini-2.5-flash-lite-preview-06-17"),
     GEMINI_FLASH_PREVIEW("Gemini 2.5 Flash", "gemini-2.5-flash"),
-    GEMINI_PRO("Gemini 2.5 Pro (maybe don't work)", "gemini-2.5-pro"),
+    GEMINI_PRO("Gemini 2.5 Pro", "gemini-2.5-pro"),
     GEMMA_3N_E4B_IT("Gemma 3n E4B it", "gemma-3n-e4b-it")
 }
 
