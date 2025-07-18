c7683837346943631e6c03e9a265e45150bd00b8
Android PowerUser <88908510+Android-PowerUser@users.noreply.github.com>
2025-07-18 11:51:57 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.google.ai.sample"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
